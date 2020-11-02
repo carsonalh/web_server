aa9b926e5f6252da0f763a7de70099c13d11bbd4
Carson Holloway <carsonalh@gmail.com>
2020-11-02 22:44:29 +1100

    Minor refactors.
    * Changed local variable naming convention from snake_case to camelCase.
    * Also changed name of Uri::Uri::Impl::Clear to clear to match the rest
    of the methods in the program.
    * Simplified some logic in the ipv6 parsing component.

--- include/uri/uri.hpp
+++ include/uri/uri.hpp
@@ -23,9 +23,9 @@ namespace Uri {
         static bool isIpv4String(const std::string& string);
 
         /**
-         * Checks if the given string is an IPv6 string or not.
+         * Checks if the given string is an IPv4 string or not.
          * @returns
-         *      True if the string matches the ipv6 format, and all the given
+         *      True if the string matches the ipv4 format, and all the given
          *      numbers are in range. False otherwise.
          */
         static bool isIpv6String(const std::string& string);
